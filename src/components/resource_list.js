84238a365e381faca68c339253d66fb6f2bb1b8a
Estelle Grésillon <estelle.gresillon@wifirst.fr>
2019-04-14 16:18:20 +0200

    Turned the class based component to a functional component

--- src/components/resource_list.js
+++ src/components/resource_list.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-class ResourceList extends React.Component {
-	render() {
-		return (
-			<div>{this.props.resource}</div>
-		)
+// class ResourceList extends React.Component {
+// 	state = { resources: [] }
+
+// 	async componentDidMount() {
+// 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+		
+// 		this.setState({ resources: response.data })
+// 	}
+
+// 	async componentDidUpdate(prevProps) {
+// 		if (prevProps.resource !== this.props.resource) {
+// 			const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+		
+// 			this.setState({ resources: response.data })
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>{this.state.resources.length}</div>
+// 		)
+// 	}
+// }
+
+const ResourceList = ({ resource }) => {
+	const [resources, setResources] = useState([])
+
+	const fetchResource = async (resource) => {
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+		
+		setResources(response.data)
 	}
+
+	// useEffect combines DidMount and DidUpdate
+	// you can click multiple times in a row on posts or todo,
+	// the request is done once anyway !
+	// ultimately, it prevents from infinites re-renderings...
+	useEffect(() => {
+		fetchResource(resource)
+	}, [resource])
+
+	return <div>{resources.length}</div>
 }
 
 export default ResourceList
